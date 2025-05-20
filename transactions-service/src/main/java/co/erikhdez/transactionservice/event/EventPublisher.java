439a415e30ef2367101bc6c919a4f59a4df30198
Erik Dario Hernandez Vasquez <erikdhv@gmail.com>
2025-05-19 22:08:52 -0500

    Se adiciona funcionalidad al metodo sendEvent

--- transactions-service/src/main/java/co/erikhdez/transactionservice/event/EventPublisher.java
+++ transactions-service/src/main/java/co/erikhdez/transactionservice/event/EventPublisher.java
@@ -1,15 +1,28 @@
 package co.erikhdez.transactionservice.event;
 
+import co.erikhdez.transactionservice.model.Transaction;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EventPublisher {
     private final RabbitTemplate rabbitTemplate;
 
+    @Value("${rabbit.queue.name}")
+    private String queueName;
+
+    @Value("${rabbit.exchange.name}")
+    private String exchangeName;
+
+    @Value("${rabbit.routing.key}")
+    private String routingKey;
+
     public EventPublisher(RabbitTemplate rabbitTemplate) {
         this.rabbitTemplate = rabbitTemplate;
     }
 
-    public void sendEvent(Transaction transaction) {}
+    public void sendEvent(Transaction transaction) {
+        rabbitTemplate.convertAndSend(exchangeName, routingKey, transaction);
+    }
 }
